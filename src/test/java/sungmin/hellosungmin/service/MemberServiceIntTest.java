7cfea9c401005af250392ee21fc99f32c7ccfa70
sungmin69355 <sungmin69355@naver.com>
2021-10-25 15:46:45 +0900

    fix: 스프링 통합 테스트 추가

---
+++ src/test/java/sungmin/hellosungmin/service/MemberServiceIntTest.java
@@ -0,0 +1,49 @@
+package sungmin.hellosungmin.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import sungmin.hellosungmin.domain.Member;
+import sungmin.hellosungmin.repository.MemberRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest //스프링 컨테이너와 테스트를 함께 실행한다.
+@Transactional //테스트 케이스에 이 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고,테스트 완료 후에 항상 롤백한다. 이렇게 하면 DB에 데이터가 남지 않으므로 다음 테스트에 영향을 주지않는다.
+public class MemberServiceIntTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spirng");
+
+        Member member2 = new Member();
+        member2.setName("spirng");
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        //assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        //then
+    }
+}
